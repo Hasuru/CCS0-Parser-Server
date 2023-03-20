6fe475501892f4d949625211f4db89cec2e30a65
Hasuru <hugo.as.silva25@gmail.com>
2023-03-20 15:05:18 +0000

    Erro pattern matching ??? have no clue

--- src/lts_server.erl
+++ src/lts_server.erl
@@ -1,23 +1,63 @@
 -module(lts_server).
--export([start/0, loop/0, parse_ccs0/1, parse/2]).
+-export([start/0, loop/0, join/1, ccs0_to_lts/4, translate/2]).
 
 %----------------------------%
 % LADO DO SERVIDOR
 
+-type ccs0() :: {string(), zero}
+              | {string(), ccs0()}
+              | {choice, ccs0(), ccs0()}.
+
+% estupido, mas funciona
+join(State) ->
+    lists:map(fun(I)->
+        list_to_atom(lists:flatten(io_lib:format("s~B", [I])))
+        end, lists:seq(State,State)).
+
 % funcao de traducao ccs0 para lts
-parse_ccs0(CCS0) -> CCS0+1.
+% especificar que recebe ccs0 como argumento
+-spec ccs0_to_lts(ccs0(), string(), number(), Res) -> Res.
+
+% Teste (que nao funciona nao sei pq)
+ccs0_to_lts(Ast, LastRead, CurState, Res) ->
+    io_lib:format(Ast),
+    Res.
+%ccs0_to_lts({State, zero}, LastRead, CurState, Res) ->
+%    [H1|_] = join(CurState),
+%    Res ++ [{H1, 
+%            list_to_atom(State), 
+%            s0}].
+%ccs0_to_lts({State, AST}, LastRead, CurState, Res) ->
+%    [H1|_] = join(CurState),
+%    [H2|_] = join(CurState+1),
+%    Res ++ 
+%    [{H1, list_to_atom(State), H2}] ++
+%    ccs0_to_lts(AST, State, CurState+1, Res).
+%ccs0_to_lts({choice, Left, Right}, LastRead, CurState, Res) ->
+%    [H1|_] = join(CurState),
+%    [H2|_] = join(CurState+1),
+%    [H3|_] = join(CurState+2),
+%    Res ++
+%    [{H1, list_to_atom(LastRead), H2}, {H1, list_to_atom(LastRead), H3}] ++
+%    ccs0_to_lts(Left, LastRead, CurState+1, Res) ++
+%    ccs0_to_lts(Right, LastRead, CurState+2, Res).
 
 start() ->
     spawn(fun() -> loop() end).
 
 loop() ->
     receive
-        % cliente pede que o servidor feche a comunicacao
+        % Pedido de paragem do servidor
         {quit, From} ->
-            From ! {response, io:format("Server will close down")};
+            ok;
 
-        {parse, From, CCS0} ->
-            From ! {response, parse_ccs0(CCS0)},
+        % Pedido sobre o status do servidor
+        {status, From} ->
+            From ! {self()},
+            loop();
+
+        {translate, From, AST} ->
+            From ! {response, ccs0_to_lts(AST, "", 1, [])},
             loop();
 
         {Other, From} ->
@@ -28,8 +68,8 @@ loop() ->
 %----------------------------%
 % LADO DO CLIENTE
 
-parse(Server, CCS0) ->
-    Server ! {parse, self(), CCS0},
+translate(Server, AST) ->
+    Server ! {translate, self(), AST},
     receive
         {response, Result} ->
             Result
